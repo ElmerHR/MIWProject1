85b7cc17a32ba9a440e46423adee5d1339bac68d
Elmer van den Dries <elmervandendries@gmail.com>
2023-03-14 14:01:21 +0100

    Edited predict page

--- run/app.py
+++ run/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request, jsonify
 from forms import getLifespanForm
 import pickle
 import numpy as np
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ebc72148568923ee1fdd713b4a247f4c97548a11a409fbfc'
 
 # load the pipeline from disk
-filename = 'finalized_model.sav'
+filename = 'finalized_model.pkl'
 pipe = pickle.load(open(filename, 'rb'))
 
 
@@ -29,14 +29,16 @@ def index():
                                 'alcohol': float(form.alcohol.data),
                                 'sugar': float(form.sugar.data)
                                 }
+        session['new_params'] = session.get('params')
         return redirect(url_for('predict'))
     return render_template('index.html', form=form)
 
 @app.route('/predict')
 def predict():
-    params = session.get('params')
+    params = session.get('new_params')
     params_copied = params.copy()
     params_copied['bmi'] = params_copied['mass'] / (params_copied['length']/100)**2
+    params['bmi'] = params_copied['bmi']
     params_copied['mass_square'] = params_copied['mass']**2
     params_copied['bmi_square'] = params_copied['bmi']**2
     params_copied['exercise_sqrt'] = np.power(params_copied['exercise'], 1/2)
@@ -50,6 +52,33 @@ def predict():
     prediction = pipe.predict(params_df)
     ic(prediction)
     return render_template('predict.html', params=params, prediction=prediction)
+
+@app.route('/ajax_prediction', methods = ['POST'])
+def ajax_request():
+    session_params = session.get('new_params')
+    ic(session_params)
+    ajax_param_name = request.form['parameter_name']
+    ajax_param_value = request.form['parameter_value']
+    # update parameter and make new prediction
+    params_copied = session_params.copy()
+    params_copied[ajax_param_name] = float(ajax_param_value)
+    session_params[ajax_param_name] = float(ajax_param_value)
+    session['new_params'] = session_params
+    params_copied['bmi'] = params_copied['mass'] / (params_copied['length']/100)**2
+    session_params['bmi'] = params_copied['bmi']
+    params_copied['mass_square'] = params_copied['mass']**2
+    session_params['mass_square'] = params_copied['mass_square']
+    params_copied['bmi_square'] = params_copied['bmi']**2
+    session_params['bmi square'] = params_copied['bmi_square']
+    params_copied['exercise_sqrt'] = np.power(params_copied['exercise'], 1/2)
+    params_copied.pop('mass')
+    ic(params_copied)
+    ic(session_params)
+    params_df = pd.DataFrame(params_copied, index=[0])
+    params_df = params_df.reindex(columns=['genetic', 'length', 'bmi', 'exercise', 'smoking', 'alcohol', 'sugar', 'mass_square', 'bmi_square', 'exercise_sqrt'])
+    print(params_df)
+    prediction = pipe.predict(params_df)
+    return jsonify(prediction=round(prediction[0], 1))
 
 if __name__ == "__main__":
     app.run()
