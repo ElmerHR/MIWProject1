09fbb87a921bc17306260802a4061fef58371251
Elmer van den Dries <elmervandendries@gmail.com>
2023-03-15 14:00:27 +0100

    Refactored cli-app based on example from Jeroen

--- run/cli.py
+++ run/cli.py
@@ -15,6 +15,25 @@ colorama_init(autoreset=True)
 filename = 'finalized_model.pkl'
 model = pickle.load(open(filename, 'rb'))
 
+def prompt_param(message, acceptable_range, default):
+    """Function to prompt for parameters
+
+    Args:
+        message (str): message to show in prompt
+        acceptable_range (tuple(int, int)): acceptable range for input
+        default (int): default value to accept as prompt
+
+    Returns:
+        float: entered value by user
+    """
+
+    while True:
+        param = click.prompt(f'Patient\'s {message} | default:', type=int, default=default)
+        if param in range(acceptable_range[0], acceptable_range[1]):
+            break
+        else:
+            click.echo(f"Please provide a number between {str(acceptable_range[0])} and {str(acceptable_range[1])}.")
+    return float(param)
 
 # init and run click
 @click.command()
@@ -38,79 +57,31 @@ def makeCLI():
     print(f"{Fore.LIGHTRED_EX}{enter_params}")
 
     # Define allowed ranges for user input
-    genetic_range = (60, 110)
-    length_range = (150, 215)
-    mass_range = (50, 165)
-    exercise_range = (0, 8)
-    alcohol_range = (0, 10)
-    smoking_range = (0, 25)
-    sugar_range = (0, 15)
-
-    while True:
-        genetic = click.prompt('Patient\'s genetic age | default:', type=int, default=85)
-        if genetic in range(genetic_range[0], genetic_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(genetic_range[0])} and {str(genetic_range[1])}.")
-    while True:
-        length = click.prompt('Patient\'s length in cm | default:', type=float, default=185)
-        if length in range(length_range[0], length_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(length_range[0])} and {str(length_range[1])}.")
-    while True:
-        mass = click.prompt('Patient\'s weight in kg | default:', type=float, default=80)
-        if mass in range(mass_range[0], mass_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(mass_range[0])} and {str(mass_range[1])}.")
-    while True:
-        exercise = click.prompt('Patient\'s exercise in hr/day | default:', type=float, default=2)
-        if exercise in range(exercise_range[0], exercise_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(exercise_range[0])} and {str(exercise_range[1])}.")
-    while True:
-        alcohol = click.prompt('Patient\'s alcohol in glasses/day | default:', type=float, default=0)
-        if alcohol in range(alcohol_range[0], alcohol_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(alcohol_range[0])} and {str(alcohol_range[1])}.")
-    while True:
-        smoking = click.prompt('Patient\'s smoking in sigarettes/day| default:', type=float, default=0)
-        if smoking in range(smoking_range[0], smoking_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(smoking_range[0])} and {str(smoking_range[1])}.")
-    while True:
-        sugar = click.prompt('Patient\'s sugar in cubes/day | default:', type=float, default=4)
-        if sugar in range(sugar_range[0], sugar_range[1]):
-            break
-        else:
-            click.echo(f"Please provide a number between {str(sugar_range[0])} and {str(sugar_range[1])}.")
+    parameters_prompts = dict()
+    entered_parameters = dict()
+    parameters_prompts['genetic'] = ["genetic age", (60, 110), 85]
+    parameters_prompts['length'] = ["length in cm", (150, 215), 185]
+    parameters_prompts['mass'] = ["Weight in kg", (50, 165), 80]
+    parameters_prompts['exercise'] = ["Exercise in hr/day", (0, 8), 2]
+    parameters_prompts['alcohol'] = ["Alcohol in glasses/day", (0, 10), 0]
+    parameters_prompts['smoking'] = ["Smoking in cig./day", (0, 25), 0]
+    parameters_prompts['sugar'] = ["Sugar in cubes/day", (0, 15), 4]
     
-    # save params to dict
-    params = {
-                'genetic': float(genetic),
-                'length': float(length),
-                'mass': float(mass),
-                'exercise': float(exercise),
-                'smoking': float(smoking),
-                'alcohol': float(alcohol),
-                'sugar': float(sugar)
-            }
+    # loop over all parameters to prompt to user
+    for k, v in parameters_prompts.items():
+        entered_parameters[k] = prompt_param(v[0], v[1], v[2])
     
     # Calculate bmi and power transforms
-    params['bmi'] = params['mass'] / (params['length']/100)**2
-    params['mass_square'] = params['mass']**2
-    params['bmi_square'] = params['bmi']**2
-    params['exercise_sqrt'] = np.power(params['exercise'], 1/2)
+    entered_parameters['bmi'] = entered_parameters['mass'] / (entered_parameters['length']/100)**2
+    entered_parameters['mass_square'] = entered_parameters['mass']**2
+    entered_parameters['bmi_square'] = entered_parameters['bmi']**2
+    entered_parameters['exercise_sqrt'] = np.power(entered_parameters['exercise'], 1/2)
 
     # remove mass from dict
-    params.pop('mass')
+    entered_parameters.pop('mass')
 
     # Convert dict to dataframe
-    params_df = pd.DataFrame(params, index=[0])
+    params_df = pd.DataFrame(entered_parameters, index=[0])
 
     # Reorder columns to match trained model
     params_df = params_df.reindex(columns=['genetic', 'length', 'bmi', 'exercise', 'smoking', 'alcohol', 'sugar', 'mass_square', 'bmi_square', 'exercise_sqrt'])
